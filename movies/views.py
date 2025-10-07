059c747b0806a50d452ccc8b7da927117c5f99ca
Nozin <Shidugovislam@mail.ru>
2025-10-07 11:06:44 +0300

    added basic views

--- movies/views.py
+++ movies/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Movie
-# class MovieList(ListView):
-#     model = Movie
-#     or
+
+class MovieList(ListView):
+    model = Movie
+    ordering = 'year'
+    template_name = 'movies.html'
+    context_object_name = 'movies'
+
+class MovieDetail(DetailView):
+    model = Movie
+    template_name = 'movie.html'
+    context_object_name = 'movie'
