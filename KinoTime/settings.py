97523d1aa64fb2b63a03d1f54e84b3834e576bcc
Nozin <Shidugovislam@mail.ru>
2025-10-07 11:25:05 +0300

    add paths in settings

--- KinoTime/settings.py
+++ KinoTime/settings.py
@@ -63,7 +63,7 @@ ROOT_URLCONF = 'KinoTime.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -130,3 +130,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
