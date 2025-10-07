5112d39d730309d618976259aab739899f8d8f9f
Nozin <Shidugovislam@mail.ru>
2025-10-07 11:08:07 +0300

    added urls for basic views

--- movies/urls.py
+++ movies/urls.py
@@ -1,2 +1,7 @@
 from django.urls import path
-from .views import
+from .views import MovieList, MovieDetail
+
+urlpatterns = [
+    path('', MovieList.as_view()),
+    path('<int:pk>', MovieDetail.as_view())
+]
