89be4fb1b942eeb09b88080c6bc787a26e97cc0a
Nozin <Shidugovislam@mail.ru>
2025-10-14 11:06:36 +0300

    created detail movie page and ref

--- movies/urls.py
+++ movies/urls.py
@@ -3,5 +3,5 @@ from .views import MovieList, MovieDetail
 
 urlpatterns = [
     path('', MovieList.as_view()),
-    path('<int:pk>', MovieDetail.as_view())
+    path('<int:pk>', MovieDetail.as_view(), name='movie_detail')
 ]
