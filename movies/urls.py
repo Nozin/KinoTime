224a6bb9901aa1146dd02869ef39ed674f2fbfc0
Nozin <Shidugovislam@mail.ru>
2025-10-14 12:07:18 +0300

    edit movie detail template

--- movies/urls.py
+++ movies/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import MovieList, MovieDetail
 
 urlpatterns = [
-    path('', MovieList.as_view()),
+    path('', MovieList.as_view(), name='movie_list'),
     path('<int:pk>', MovieDetail.as_view(), name='movie_detail')
 ]
--- movies/views.py
+++ movies/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
-from .models import Movie
+from .models import Movie, Review
 
 class MovieList(ListView):
     model = Movie
@@ -12,3 +12,7 @@ class MovieDetail(DetailView):
     model = Movie
     template_name = 'movie.html'
     context_object_name = 'movie'
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['reviews'] = Review.objects.filter(movie=self.object)#self.review_set.all() #Review.objects.order_by('-dataCreation').filter(commentPost_id=self.id)
+        return context
