a2a8c20897021807c4a757304139ed6819646eb7
Nozin <Shidugovislam@mail.ru>
2025-10-31 10:15:54 +0300

    added custom tag file

--- movies/templatetags/custom_filters.py
+++ movies/templatetags/custom_filters.py
@@ -5,7 +5,7 @@ register = template.Library()
 banwords = [
 
 ]
-
+@register.filter()
 def review_censor(value):
 
     return value
---
+++ movies/templatetags/custom_tags.py
@@ -0,0 +1,11 @@
+from django import template
+from datetime import datetime
+
+register = template.Library()
+
+banwords = [
+
+]
+@register.simple_tag()
+def current_datetime(format_str='%b %d %Y'):
+    return datetime.utcnow.strftime(format_str)
