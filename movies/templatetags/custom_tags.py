d3a3f8cdddc383e06443773b077a56e398c34e4b
Nozin <Shidugovislam@mail.ru>
2025-11-01 10:44:52 +0300

    fix(movie): fix filter selection drop when use paginator

--- movies/templatetags/custom_tags.py
+++ movies/templatetags/custom_tags.py
@@ -9,3 +9,12 @@ banwords = [
 @register.simple_tag()
 def current_datetime(format_str='%b %d %Y'):
     return datetime.utcnow().strftime(format_str)
+
+@register.simple_tag(takes_context=True)
+def url_replace(context, **kwargs):
+   d = context['request'].GET.copy()
+   print(d)
+   for k, v in kwargs.items():
+       d[k] = v
+       print(d[k])
+   return d.urlencode()
