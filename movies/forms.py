17610d6e684d7b0ce280ccb9d3dddd66b525b874
Nozin <Shidugovislam@mail.ru>
2025-11-01 11:09:11 +0300

    feature(movie): add some validator for movie create form

--- movies/forms.py
+++ movies/forms.py
@@ -1,8 +1,10 @@
 from django import forms
 from .models import Movie
-
+from django.core.exceptions import ValidationError
 
 class MovieForm(forms.ModelForm):
+    description = forms.CharField(min_length=5)
+    name = forms.CharField(min_length=2)
     class Meta:
         model = Movie
         fields = [
@@ -13,5 +15,22 @@ class MovieForm(forms.ModelForm):
             'type',
             'category',
         ]
+    def clean(self):
+        cleaned_data=super().clean()
+        name=cleaned_data.get('name')
+        description = cleaned_data.get('description')
+        if name == description:
+            raise ValidationError({
+                "description": "Название не может совпадать с описанием"
+            })
+        if name in not None and name[0].islower():
+            raise ValidationError({
+                "description": "Название должно начинаться с большой буквы"
+            })
+        if description in not None and description[0].islower():
+            raise ValidationError({
+                "description": "Описание должно начинаться с большой буквы"
+            })
 
+        return cleaned_data
 
