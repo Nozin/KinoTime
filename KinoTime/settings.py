3797f7fbb29cc48537beb8d268ffd695416496c9
Nozin <Shidugovislam@mail.ru>
2025-11-07 09:46:13 +0300

    fix(settings): add redirect urls for login

--- KinoTime/settings.py
+++ KinoTime/settings.py
@@ -122,7 +122,13 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
+
+LANGUAGES = [
+    ('en', 'English'),
+    ('ru', 'Русский'),
+]
+
 
 TIME_ZONE = 'UTC'
 
@@ -157,7 +163,9 @@ AUTHENTICATION_BACKENDS = [
     'allauth.account.auth_backends.AuthenticationBackend',
 ]
 
-#LOGIN_URL = '/accounts/login/'
+LOGIN_URL = '/accounts/login/'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 SITE_ID = 1
 
 ACCOUNT_EMAIL_REQUIRED = True
